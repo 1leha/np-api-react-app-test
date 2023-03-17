0bd6b1926f7a04a224d69f5af629ad4fe6cfa920
Alexey Lisovoy <88471482+1leha@users.noreply.github.com>
2023-03-18 00:09:53 +0200

    fix some layouts

--- src/components/App/App.styled.js
+++ src/components/App/App.styled.js
@@ -1,5 +1,7 @@
 import styled from 'styled-components';
 
 export const StyledApp = styled.div`
+  width: 100%;
+  height: 500px;
   font-family: ${p => p.theme.fonts.body};
 `;
--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,5 +1,6 @@
 import { Box } from 'components/common/Box';
 import { FullscreenSpiner } from 'components/common/Spiners/Spiner';
+import Footer from 'components/Footer';
 import Header from 'components/Header';
 import Main from 'components/Main';
 import React, { Suspense } from 'react';
@@ -7,24 +8,22 @@ import { Outlet } from 'react-router';
 
 const Layout = () => {
   return (
-    <Box width="100%">
-      <Box
-        height="100%"
-        display="flex"
-        justifyContent="space-between"
-        alignItems="center"
-        flexDirection="column"
-      >
-        <Header />
-        <Main>
-          {
-            <Suspense fallback={<FullscreenSpiner />}>
-              <Outlet />
-            </Suspense>
-          }
-        </Main>
-        {/* <Footer /> */}
-      </Box>
+    <Box
+      height="100vh"
+      display="flex"
+      justifyContent="space-between"
+      alignItems="center"
+      flexDirection="column"
+    >
+      <Header />
+      <Main>
+        {
+          <Suspense fallback={<FullscreenSpiner />}>
+            <Outlet />
+          </Suspense>
+        }
+      </Main>
+      <Footer />
     </Box>
   );
 };
